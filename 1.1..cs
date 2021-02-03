a7111237ee7d6494d705e1d53a9b03789ad21eb0
marincostak <71758033+marincostak@users.noreply.github.com>
2021-02-04 00:47:10 +0300

    Add files via upload

--- 1.1..cs
+++ 1.1..cs
@@ -7,8 +7,7 @@ namespace ConsoleApp1
         static public int Example(int x)
         {
             int c = Convert.ToInt32(Math.Pow(x, 3)) - 4 * Convert.ToInt32(Math.Pow(x, 2)) + 5 * x + 9 + Convert.ToInt32(Math.Cos(x));
-            Console.WriteLine(Math.Pow(x, 3) + " - " + 4 * Math.Pow(x, 2) + " + " + 5 * x + 9 + Math.Cos(x) + " = " +
-            c.ToString());
+            Console.WriteLine(Math.Pow(x, 3) - 4 * Math.Pow(x, 2) + 5 * x + 9 + Math.Cos(x) + " = " + c.ToString());
             return c;
         }
         static void Main(string[] args)
